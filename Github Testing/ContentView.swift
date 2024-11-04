5841a16596f6b7d152df3816511986148d59ec8e
aunmirza <aunmirza50@gmail.com>
2024-11-04 15:21:56 +0100

    Initial Commit

---
+++ Github Testing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Github Testing
+//
+//  Created by Muhammad Aun e Ali Mirza on 04/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Github Testing/Github_TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_TestingApp.swift
+//  Github Testing
+//
+//  Created by Muhammad Aun e Ali Mirza on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Github_TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
